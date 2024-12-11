b1ffff8a09c84bce51749534f5c80ae279738ecc
ismailhasir <ismailhasir@gmail.com>
2024-12-11 10:03:49 +0300

    minor fixes

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ const shareButtonActive = document.querySelector(".share-active-button");
 const footer = document.querySelector(".footer");
 const footerActive = document.querySelector(".footer-2");
 
+// Detect desktop or mobile
+const isDesktop = window.matchMedia("(min-width: 750px)").matches;
+
 shareButton.addEventListener("click", (e) => {
-  footerActive.classList.remove("hidden");
-  footer.classList.add("hidden");
+  if (isDesktop) {
+    console.log("Desktop!!");
+  } else {
+    footerActive.classList.remove("hidden");
+    footer.classList.add("hidden");
+  }
 });
 
 shareButtonActive.addEventListener("click", (e) => {
-  footerActive.classList.add("hidden");
-  footer.classList.remove("hidden");
+  if (isDesktop) {
+    console.log("BBBBBB");
+  } else {
+    footerActive.classList.add("hidden");
+    footer.classList.remove("hidden");
+  }
 });
